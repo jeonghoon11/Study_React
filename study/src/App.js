c701404fa164385442eaab46e91778a81d2c5b44
jeonghoon11 <jih361@naver.com>
2024-11-03 01:00:36 +0900

    Router

--- study/src/App.js
+++ study/src/App.js
@@ -1,12 +1,24 @@
 import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
+import {  BrowserRouter, Route, Routes  } from 'react-router-dom';
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
-  return <div className="App">
-    <Header />
-    <DayList />
-    <Day />
-  </div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+            <Route path="/" element={<DayList />} />
+            <Route path="/day/:day" element={<Day />} />
+            <Route element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
+
+
+
 export default App;
--- study/src/component/Day.js
+++ study/src/component/Day.js
@@ -1,21 +1,24 @@
 import dummy from "../db/data.json";
-
+import { useParams } from "react-router-dom";
 export default function Day() {
     // dummy.words
-    const day = 1;
-    const wordList = dummy.words.filter(word => word.day === day);
-    console.log(wordList)
-
-    return <>
-    <table>
-        <tbody>
-            {wordList.map(word => (
-                <tr key={word.id}>
-                    <td>{word.eng}</td>
-                    <td>{word.kor}</td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
-    </>;
+    const {day} = useParams();
+    const wordList = dummy.words.filter(word => word.day === Number(day));
+    
+    return (
+    <>
+         <h2>Day {day}</h2>
+        <table>
+            <tbody>
+                {wordList.map(word => (
+                    <tr key={word.id}>
+                        <td>{word.eng}</td>
+                        <td>{word.kor}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </>
+    );
+    
 }
